a2f411c909e0026dc4f10c7a05f731ce7374af79
Jit <jitendrasoni24@gmail.com>
2017-11-29 17:28:20 +0000

    init s-oauth

---
+++ s-oauth/src/main/java/com/dtech/sauth/api/config/ApplicationConfig.java
@@ -0,0 +1,36 @@
+package com.dtech.sauth.api.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+/* Indicates that a class declares one or more @Bean methods and may be processed by the 
+ * Spring container to generate bean definitions and service requests for those beans at runtime */
+@Configuration
+
+/* Enable Spring MVC, content-type and accept header, generally content negotiation requires this */
+@EnableWebMvc
+
+/* Configures component scanning directives for use with @Configuration classes */
+@ComponentScan(basePackages = "com.dtech.sauth.api")
+
+/* Enables support for handling components marked with AspectJ's @Aspect annotation */
+@EnableAspectJAutoProxy
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+
+	public ApplicationConfig() {
+		System.out.println(" Step - 2 - Initialize beans ApplicationConfig");
+	}
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
---
+++ s-oauth/src/main/java/com/dtech/sauth/api/controller/AppController.java
@@ -0,0 +1,29 @@
+package com.dtech.sauth.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dtech.sauth.api.App;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+
+@RestController
+public class AppController {
+
+	public static final Faker fake = Faker.instance();
+
+	@Autowired 
+	ObjectMapper mapper;
+	
+	@GetMapping(value = "/app", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String app() throws Exception {
+		return getJsonPayload(new App("s-auth", "0.0.1"));
+	}
+	
+    private String getJsonPayload(Object object) throws JsonProcessingException {
+        return mapper.writeValueAsString(object);
+    }
+}
